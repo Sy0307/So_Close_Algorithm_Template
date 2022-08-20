3d7a7a0a76a3d703d2c4791eeecd279be3896bf6
lintkey <lintkey@outlook.com>
2022-08-20 18:47:19 +0800

    ✨ feat(杂项): 添加快读(已展开本地头文件宏)

--- 杂项/带缓冲快读.cpp
+++ 杂项/带缓冲快读.cpp
@@ -1,16 +1,41 @@
-#include <cctype>
-#include <cstdio>
-// 取消注释并删除下一行可开启缓冲(针对大文件)
-#define gc() getchar()
-// const int MAXB = 1 << 20;
-// char fbuf[MAXB], *fr = fbuf, *ed = fbuf;
-// #define fread() (ed=(fr=fbuf-1)+fread(fbuf,1,MAXB,stdin),fr==ed)
-// inline int gc() { return (fr == ed && fread()) ? EOF : *++fr; }
-// #undef fread
-template<class T>
-inline T &fin(T &v) {
-    int c = gc(); bool f = v = 0;
-    while(!isdigit(c)) f = (c = gc()) == '-';
-    for(; isdigit(c); c = gc()) v = v * 10 + c - '0';
-    return f ? v = -v : v;
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int MAXB = 1 << 20;
+char ibuf[MAXB], *ifr = ibuf, *ied = ibuf;
+#define fread() (ied=(ifr=ibuf)+fread(ibuf,1,MAXB,stdin),ifr==ied) // 单次fread不会等待输入
+#define ischar(c) (!isspace(c))
+inline int gc() { return (ifr==ied && fread()) ? EOF : *(ifr++); } // 读取并弹出字符(类似getchar)
+inline int gr() { return (ifr==ied && fread()) ? EOF : *ifr; } // 读取字符
+inline int fit() { while(isspace(gr())) ++ifr; return gr(); } // 将空字符丢弃，并返回第一个非空字符或EOF
+#undef fread
+
+template<class T> inline T &fin(T &v) {
+    int c = v = 0; bool f = fit()=='-'; ifr += f;
+    while(isdigit(c=gc())) v = v*10 + (c ^ 48);
+    return f ? (v=-v) : v;
+}
+
+// 读取非空字符(建议手动gc()，这个只是简写)
+template<> inline char &fin(char &v) { return fit(), v = gc(); }
+
+// C++式字符串，如情况允许，建议使用C式字符串
+template<> inline string &fin(string &str) {
+    str.clear();
+    for(char *fp; fit()!=EOF;) {
+        for(fp=ifr; fp<ied&&ischar(*fp); ++fp){}
+        str.append(ifr, fp - ifr); // O(n)
+        if((ifr=fp) != ied) break;
+    } return str;
+}
+
+// C式字符串
+inline char *fin(char *ch) {
+    char *cp = ch;
+    for(char *fp; fit()!=EOF;) {
+        for(fp=ifr; fp<ied&&ischar(*fp); ++fp){}
+        const int SZ = fp - ifr;
+        memcpy(cp, ifr, SZ*sizeof(char)); cp += SZ;
+        if((ifr=fp) != ied) break;
+    } return *cp = '\0', ch;
 }
